0f6703b5328855796613cc39c6731332a2060cb1
Nep <spica@starry.blue>
2016-03-30 16:28:56 +0900

    Add: Dynamic Plugin Loader! & Full-Python 3.5.x Support

--- Main.py
+++ Main.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
-import imp, re, urllib, json, threading, logging, random, os, datetime, time
-import tweepy
-import yaml
+import re, urllib, json, threading, logging, random, os, datetime, time
+from importlib import machinery
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import tweepy, yaml
 
-"""初期化する関数: プラグインを再読み込みする"""
-def InitializePlugins():
+"""プラグインを初期化する関数"""
+def InitializePlugins(init=True):
 	plugins = {}
 	"""PLUGIN_DIRから拡張機能読み込み"""
 	for plugin_file in os.listdir(PLUGIN_DIR):
 		if re.compile('[^.].*\.py$').match(plugin_file):
 			name = plugin_file[:-3]
 			try:
-				ext_info = imp.find_module(name, [PLUGIN_DIR])
-				plugin = imp.load_module(name, *ext_info)
-				plugin.NAME = name
-				plugins[name] = plugin
-				logging.info(u'プラグイン「%s」は有効になりました。' % name)
-			except:
-				logging.info(u'プラグイン「%s」は壊れています。有効にできませんでした。文法を確認してください。' % name)
+				loader = machinery.SourceFileLoader(name, PLUGIN_DIR+'/'+name+'.py')
+				module = loader.load_module(name)
+				module.NAME = name
+				plugins[name] = module
+				logger.info('プラグイン「%s」は有効になりました。' % name)
+			except Exception as e:
+				logger.warning('プラグイン「%s」は壊れています。有効にできませんでした。\nエラーログ: %s' % (name, e))
 	"""pluginの種類を分類"""
+	if init == False:
+		global reply_plugin, timeline_plugin, event_plugin, thread_plugin, regular_plugin
+		reply_plugin, timeline_plugin, event_plugin, thread_plugin, regular_plugin = [], [], [], [], []
 	for i in plugins:
 		plugin = plugins[i]
 		if plugin.TARGET == 'REPLY':
@@ -30,15 +35,7 @@ def InitializePlugins():
 		elif plugin.TARGET == 'THREAD':
 			thread_plugin.append(plugin)
 		elif plugin.TARGET == 'REGULAR':
-			thread_plugin.append(plugin)
-	logging.info(u"""プラグインの一覧:
-						リプライプラグイン: %s
-						タイムラインプラグイン: %s
-						イベントプラグイン: %s
-						スレッドプラグイン: %s
-						定期実行プラグイン: %s
-						"""
-				 % (str(reply_plugin), str(timeline_plugin), str(event_plugin), str(thread_plugin), str(regular_plugin)))
+			regular_plugin.append(plugin)
 
 """プラグインの結果をツイートする関数"""
 def Post(text, stream, dm=False, lat=None, longs=None, in_reply_to_status_id=None, filename=None):
@@ -46,18 +43,18 @@ def Post(text, stream, dm=False, lat=None, longs=None, in_reply_to_status_id=Non
 		if dm == True:
 			API.send_direct_message(screen_name=stream['user']['screen_name'], text=text)
 		elif filename == None:
-			API.update_status(status=text, in_reply_to_status_id=in_reply_to_status_id, lat=lat, long=longs)
+			API.update_status(status=text, in_reply_to_status_id=in_reply_to_status_id, lat=lat, int=longs)
 		else:
-			API.update_with_media(filename=filename, status=text, in_reply_to_status_id=in_reply_to_status_id, lat=lat, long=longs)
+			API.update_with_media(filename=filename, status=text, in_reply_to_status_id=in_reply_to_status_id, lat=lat, int=longs)
 		return True
-	except tweepy.TweepError, e:
+	except tweepy.TweepError as e:
 		code = e[0]
 		if code == 186: # 186 = Status is over 140 characters
-			Post(u'@%s ツイートが長すぎるため、DMで送信しました。' % stream['user']['screen_name'], stream, dm=False, lat=lat, longs=longs, in_reply_to_status_id=in_reply_to_status_id, filename=filename)
+			Post('@%s ツイートが長すぎるため、DMで送信しました。' % stream['user']['screen_name'], stream, dm=False, lat=lat, longs=longs, in_reply_to_status_id=in_reply_to_status_id, filename=filename)
 			Post(text, stream, dm=True, lat=lat, longs=longs, in_reply_to_status_id=in_reply_to_status_id, filename=filename)
 			return True
 		elif code in [150, 187, 226]:
-			logging.warning(u'Twitter APIエラーが発生しました\n詳細: %s' % ' '.join(e))
+			logger.warning('Twitter APIエラーが発生しました\n詳細: %s' % ' '.join(e))
 			#150 = only you can send messages to who follows you(DM)
 			#187 = Status is a duplicate
 			#226 = your request was automated
@@ -72,7 +69,7 @@ def StreamLine(raw):
 		if 'text' in stream:
 			stream['source'] = re.sub('<.*?>','', stream['source'])
 			#名前欄攻撃対策
-			stream['user']['name'] = stream['user']['name'].replace('@', u'@​')
+			stream['user']['name'] = stream['user']['name'].replace('@', '@​')
 
 			if re.match('@%s\s' % SN, stream['text'], re.IGNORECASE):
 				for plugin in reply_plugin:
@@ -86,7 +83,7 @@ def StreamLine(raw):
 				ExecutePlugin(plugin, stream)
 
 	except Exception as e:
-		print(e)
+		logger.warning('UserStreamでエラーが発生しました\n詳細: %s' % ' '.join(e))
 
 """プラグインを実行する関数"""
 def ExecutePlugin(plugin, stream):
@@ -96,7 +93,18 @@ def ExecutePlugin(plugin, stream):
 		else:
 			ratio = 1
 		if random.randint(1, ratio) == 1: #1/RATIOの確率でプラグイン実行
-			result = plugin.do(stream)
+			if plugin.do.__code__.co_argcount == 2:
+				#引数の数が2の場合、グローバル変数を渡す
+				gvars = {"setting": setting,
+								"logger": logger,
+								"api": API,
+								"auth": auth,
+								"path": {"script": __file__, "plugin_dir": PLUGIN_DIR, "cache": CACHE_PARH, "setting": SETTING_PATH, "work_dir": WORK_DIR, "log": LOG_PATH},
+								"plugin": {"reply": reply_plugin, "timeline": timeline_plugin, "event": event_plugin, "thread": thread_plugin, "regular": regular_plugin}
+								}
+				result = plugin.do(stream, gvars)
+			else:
+				result = plugin.do(stream)
 		else: #そうでなければ終了
 			return True
 		if result:
@@ -114,15 +122,16 @@ def ExecutePlugin(plugin, stream):
 					if 'filename' in result: filename = result['filename']
 					else: filename = None
 					Post(text, stream, dm, lat, longs, in_reply_to_status_id, filename)
-			else:
-				logging.debug(u'プラグイン "%s" の返り値が不正です\nプラグインは辞書かFalseをrerurnしてください' % plugin.NAME)
+				if 'update' in result:
+					#updateの中身を統合
+					CACHE.update(result['update'])
 		elif result == False:
 			raise Exception
 	except Exception as e:
 		if plugin.TARGET == 'REPLY': #リプライプラグインの時のみユーザーにエラーを知らせる
-			text = u'@%s プラグイン "%s"でエラーが発生しました。\n\n詳細: %s' % (stream['user']['screen_name'], plugin.NAME, e)
+			text = '@%s プラグイン "%s"でエラーが発生しました。\n\n詳細: %s' % (stream['user']['screen_name'], plugin.NAME, e)
 			Post(text, stream)
-		logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+		logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 
 """tweepyのクラスを継承してUserStreamを受信するクラス"""
 class StreamListener(tweepy.StreamListener):
@@ -133,13 +142,13 @@ class StreamListener(tweepy.StreamListener):
 			t.join(20) #スレッドのタイムアウト 20秒
 			if t.isAlive():
 				t.__stop()
-				logging.warning(u'スレッドがタイムアウトしました: %s' % raw)
+				logger.warning('スレッドがタイムアウトしました: %s' % raw)
 			return True
 		else:
-			logging.warning(u'解析不能なUserStreamデータを受信しました: %s' % raw)
+			logger.warning('解析不能なUserStreamデータを受信しました: %s' % raw)
 			return False
 	def on_error(self, status_code):
-		print(status_code)
+		logger.warning('Twitter APIエラーが発生しました\n詳細: HTTPステータスコード=%s' % status_code)
 
 """tweepyのクラスを継承してUserStreamの設定をするクラス"""
 class UserStream(tweepy.Stream):
@@ -149,7 +158,7 @@ class UserStream(tweepy.Stream):
 		self.scheme = "https"
 		self.host = 'userstream.twitter.com'
 		self.url = '/1.1/user.json'
-		self.body = urllib.urlencode(self.parameters)
+		self.body = urllib.parse.urlencode(self.parameters)
 		self.timeout = None
 		self._start(False)
 
@@ -183,21 +192,21 @@ class ScheduleTask(threading.Thread):
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
 				elif hour != None:
 					if hour == datetime_hour: #時だけ指定型(その時だけ毎分実行)
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
 				elif minute != None:
 					if minute == datetime_minute: #分だけ指定型(その分だけ毎時実行)
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
 				#ある倍数の時だけ実行する
 				if 'MULTIPLE_HOUR' in dir(plugin):
@@ -213,46 +222,91 @@ class ScheduleTask(threading.Thread):
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
 				elif hour != None:
 					if datetime_hour % hour == 0: #時だけ倍数指定型(その倍数時だけ毎分実行)
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
 				elif minute != None:
 					if datetime_minute % minute == 0: #分だけ倍数指定型(その倍数分だけ毎時実行)
 						try:
 							plugin.do()
 						except Exception as e:
-							logging.warning(u'プラグイン "%s" でエラーが発生しました\n詳細: %s' % plugin.NAME, e)
+							logger.warning('プラグイン "%s" でエラーが発生しました\n詳細: %s' % (plugin.NAME, e))
 						continue
+
+"""プラグインディレクトリを監視するハンドラ"""
+class ChangeHandler(FileSystemEventHandler):
+	def on_created(self, event):
+		if event.is_directory:
+			return
+		if not event.src_path.endswith('.py'):
+			return
+		name = event.src_path[:-3].replace(PLUGIN_DIR+'/', '')
+		try:
+			loader = machinery.SourceFileLoader(name, event.src_path)
+			module = loader.load_module(name)
+			module.NAME = name
+			if module.TARGET == 'REPLY':
+				reply_plugin.append(module)
+			elif module.TARGET == 'TIMELINE':
+				timeline_plugin.append(module)
+			elif module.TARGET == 'EVENT':
+				event_plugin.append(module)
+			elif module.TARGET == 'THREAD':
+				thread_plugin.append(module)
+			elif module.TARGET == 'REGULAR':
+				regular_plugin.append(module)
+			logger.info('プラグイン「%s」は有効になりました。' % name)
+		except Exception as e:
+			logger.warning('プラグイン「%s」は壊れています。有効にできませんでした。\nエラーログ: %s' % (name, e))
+
+	def on_modified(self, event):
+		if event.is_directory:
+			return
+		if not event.src_path.endswith('.py'):
+			return
+		InitializePlugins(init=False)
+
+	def on_deleted(self, event):
+		if event.is_directory:
+			return
+		if not event.src_path.endswith('.py'):
+			return
+		InitializePlugins(init=False)
 
 if __name__ == '__main__':
 	"""設定データを読み込み"""
+	global  SETTING_PATH
 	SETTING_PATH = os.path.dirname(os.path.abspath(__file__)) + '/setting.yaml'
 	setting = yaml.load(open(SETTING_PATH).read())
 
 	""""パスを読み込み"""
+	global  PLUGIN_DIR, WORK_DIR, CACHE_PARH, LOG_PATH
 	PLUGIN_DIR = setting['PLUGIN_DIR']
 	WORK_DIR = setting['WORK_DIR']
+	CACHE_PARH = WORK_DIR + '/cache.json'
 	LOG_PATH = setting['LOG_DIR']+'/' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.log'
 
 	"""ロガーを準備"""
-	logging.basicConfig(level=logging.INFO, filename=LOG_PATH, format='[%(levelname)s]%(asctime)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+	global logger
+	handler = logging.FileHandler(LOG_PATH, 'w', encoding='UTF-8')
+	formatter = logging.Formatter('[%(levelname)s]%(asctime)s - %(message)s')
+	handler.setFormatter(formatter)
+	logger = logging.getLogger()
+	logger.addHandler(handler)
+	logger.setLevel(logging.INFO)
 
 	"""プラグインを格納するリストを定義"""
-	global reply_plugin #@リプライに反応するプラグイン
+	global reply_plugin, timeline_plugin, event_plugin, thread_plugin, regular_plugin
 	reply_plugin = []
-	global timeline_plugin #タイムラインに反応するプラグイン
 	timeline_plugin = []
-	global event_plugin #UserStreamイベントに反応するプラグイン
 	event_plugin = []
-	global thread_plugin #スレッドで起動するプラグイン
 	thread_plugin = []
-	global regular_plugin #定期実行するプラグイン
 	regular_plugin = []
 
 	"""UserStreamに接続するアカウントの認証"""
@@ -265,10 +319,32 @@ if __name__ == '__main__':
 	"""プラグイン初期化"""
 	InitializePlugins()
 
+	""""キャッシュ or データベース"""
+	global CACHE
+	CACHE = {}
+	#保管済のキャッシュがあれば読み込み
+	try:
+		CACHE = eval(open(CACHE_PARH, 'r').read())
+		logger.info('前回のキャッシュを読み込みました。')
+	except:
+		pass
+
 	"""別スレッドで処理するスレッドを起動"""
 	[x.do().start() for x in thread_plugin]
 	ScheduleTask().start()
 
+	""""プラグインディレクトリを監視"""
+	event_handler = ChangeHandler()
+	observer = Observer()
+	observer.schedule(event_handler, PLUGIN_DIR, recursive=False)
+	observer.start()
+
+	logger.info('起動完了')
+
 	"""UserStreamに接続"""
 	while True:
-		UserStream(auth, StreamListener()).user_stream()
+		try:
+			UserStream(auth, StreamListener()).user_stream()
+		except:
+			logger.warning('UserStreamから切断されました。10秒後に再接続します。')
+			time.sleep(10)
