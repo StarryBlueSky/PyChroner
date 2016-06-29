392c696180a26d8e8fd9b05d0243c15975686121
Nep <spica@starry.blue>
2016-06-30 00:59:52 +0900

    Add: init

---
+++ TBFW/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+"""
+TBFW library
+"""
+__version__ = '2.0.0'
+__author__ = 'Nephy Project Team'
+__license__ = 'MIT'
+
+from TBFW.core import Core
+from TBFW.api import Plugin
+
+if __name__ == "__main__":
+	pass
