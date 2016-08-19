754dfbfc2c8177695fbd90f788442d5c8a5697f6
Nep <spica@starry.blue>
2016-08-19 15:21:19 +0900

    Add: plugin exception

--- TBFW/exceptions.py
+++ TBFW/exceptions.py
@@ -1,14 +1,13 @@
 # coding=utf-8
 
 class GeneralError(Exception):
-	"""
-	raise general exceptions
-	"""
 	raise Exception
 
 class OutOfMemoryError(Exception):
-	"""
-	raise MemoryError
-	"""
-	raise MemoryError("TBFW stoped because of out of memory")
+	raise MemoryError("TBFW stoped because of out of memory.")
 
+class InValidPluginFilenameError(Exception):
+	raise GeneralError("TBFW does not support that plugin's extension. Please check plugin's extension.")
+
+class InvalidPluginSyntaxError(Exception):
+	raise SyntaxError("TBFW could not load plugin because of invalid syntax.")
