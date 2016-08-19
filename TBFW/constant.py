95e87abfd592a08f8e09de74e01fdd64ae087ed4
Nep <spica@starry.blue>
2016-08-19 20:25:07 +0900

    Add: plugin constants

--- TBFW/constant.py
+++ TBFW/constant.py
@@ -1,10 +1,43 @@
 # coding=utf-8
 
-pluginTypes = [
-	"reply", "timeline", "event", "thread", "regular", "other"
-]
-
 pluginsDir = "plugins"
 assetsDir = "assets"
+cacheDir = "cache"
 logDir = "logs"
 tmpDir = "tmp"
+
+pluginReply = "reply"
+pluginTimeline = "timeline"
+pluginEvent = "event"
+pluginThread = "thread"
+pluginRegular = "regular"
+pluginOther = "other"
+
+pluginTypes = [
+	pluginReply,
+	pluginTimeline,
+	pluginEvent,
+	pluginThread,
+	pluginRegular,
+	pluginOther
+]
+
+pluginAttributeTarget = "TARGET"
+pluginAttributePriority = "PRIORITY"
+pluginAttributeAttachedStream = "STREAM"
+pluginAttributeRatio = "RATIO"
+pluginAttributeHour = "HOUR"
+pluginAttributeMultipleHour = "MULTIPLE_HOUR"
+pluginAttributeMinute = "MINUTE"
+pluginAttributeMultipleMinute = "MULTIPLE_MINUTE"
+
+defaultAttributePath = None
+defaultAttributeName = None
+defaultAttributeTarget = None
+defaultAttributePriority = 0
+defaultAttributeAttachedStream = 0
+defaultAttributeRatio = 1
+defaultAttributeHour = None
+defaultAttributeMultipleHour = None
+defaultAttributeMinute = None
+defaultAttributeMultipleMinute = None
