8a5e9e20b985841837fa18c28c1f70376620b6ed
Nep <spica@starry.blue>
2016-08-19 15:21:01 +0900

    Improve: load module

--- TBFW/__init__.py
+++ TBFW/__init__.py
@@ -3,14 +3,22 @@
 """
 TBFW library
 """
-__version__ = '2.0.0'
+
+__version__ = '3.0.0'
 __author__ = 'Nephy Project Team'
 __license__ = 'MIT'
 
 from TBFW.core import Core
 from TBFW.database import DBProvider
-from TBFW.api import Plugin
-from TBFW.exceptions import GeneralError, OutOfMemoryError
+from TBFW.api import API
+from TBFW.plugin import Plugin, PluginManager
+from TBFW.exceptions import *
+from TBFW.constant import *
+
+__all__ = ["Core"]
+
+import logging
 
 if __name__ == "__main__":
-	pass
+	logger = logging.getLogger(__name__)
+	logger.debug("Module has loaded.")
