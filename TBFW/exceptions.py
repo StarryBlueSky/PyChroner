77fe2820fe095ef60abaa5552b0ae043a289ea87
Nep <spica@starry.blue>
2016-08-19 17:54:47 +0900

    Add: NotFoundPluginTargetError

--- TBFW/exceptions.py
+++ TBFW/exceptions.py
@@ -7,10 +7,13 @@ class OutOfMemoryError(Exception):
 	raise MemoryError("TBFW stoped because of out of memory.")
 
 class InValidPluginFilenameError(Exception):
-	raise Exception("TBFW does not support that plugin's extension. Please check plugin's extension.")
+	raise FileNotFoundError("TBFW does not support that plugin's extension. Please check plugin's extension.")
+
+class NotFoundPluginTargetError(Exception):
+	raise SyntaxError("TBFW could not load plugin because of lacking of `TARGET` variable.")
 
 class InvalidPluginTargetError(Exception):
-	raise SyntaxError("TBFW could not load plugin because of unsupported target.")
+	raise NotImplementedError("TBFW could not load plugin because of unsupported target.")
 
 class InvalidPluginSyntaxError(Exception):
 	raise SyntaxError("TBFW could not load plugin because of invalid syntax.")
