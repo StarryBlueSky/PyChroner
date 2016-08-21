d5a926d7188b1a1fc1e385013b491f00a73d2a17
Nep <spica@starry.blue>
2016-08-21 15:17:41 +0900

    Move: startUserStream method

--- TBFW/twitterapi.py
+++ TBFW/twitterapi.py
@@ -1,39 +1,14 @@
 # coding=utf-8
-import tweepy
-import time
-import threading
 import logging
 import urllib.parse
-from TBFW.constant import *
+
+import tweepy
+
 logger = logging.getLogger(__name__)
 
 class TwitterAPI:
 	def __init__(self):
 		pass
-
-	def startUserStream(self, n):
-		auth = tweepy.OAuthHandler(Set['twitterSecret'][n]['ck'], Set['twitterSecret'][n]['cs'])
-		auth.set_access_token(Set['twitterSecret'][n]['at'], Set['twitterSecret'][n]['ats'])
-		sn = Set['twitterSecret'][n]['screen_name']
-		while True:
-			try:
-				logger.info('@%sのUserStreamに接続しました。' % sn)
-				UserStream(auth, StreamListener(n, sn)).user_stream()
-			except:
-				logger.warning('@%sのUserStreamから切断されました。10秒後に再接続します。エラーログ: \n%s' % (sn, traceback.format_exc()))
-				time.sleep(reconnectUserStreamSeconds)
-
-class StreamListener(tweepy.StreamListener):
-	def __init__(self, n, sn):
-		self.n = n
-		self.sn = sn
-
-	def on_data(self, raw):
-		t = threading.Thread(target=StreamLine, name='StreamLine', args=(raw, self.n, self.sn))
-		t.start()
-
-	def on_error(self, status_code):
-		logger.warning('Twitter APIエラーが発生しました\n詳細: HTTPステータスコード=%s' % status_code)
 
 class UserStream(tweepy.Stream):
 	def user_stream(self):
