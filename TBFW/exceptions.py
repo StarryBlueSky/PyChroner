16228320601909b373a2d02cbcc5b280aed08376
Nep <spica@starry.blue>
2016-08-21 14:05:28 +0900

    Improve: exceptions

--- TBFW/exceptions.py
+++ TBFW/exceptions.py
@@ -3,23 +3,50 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class OutOfMemoryError(Exception):
-	raise MemoryError("TBFW stoped because of out of memory.")
-
 class InValidPluginFilenameError(Exception):
-	raise FileNotFoundError("TBFW does not support that plugin's extension. Please check plugin's extension.")
+	def __init__(self):
+		self.message = "TBFW does not support that plugin's extension. Please check plugin's extension."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
 
 class NotFoundPluginTargetError(Exception):
-	raise SyntaxError("TBFW could not load plugin because of lacking of `TARGET` variable.")
+	def __init__(self):
+		self.message = "TBFW could not load plugin because of lacking of `TARGET` variable."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
 
 class InvalidPluginTargetError(Exception):
-	raise NotImplementedError("TBFW could not load plugin because of unsupported target.")
+	def __init__(self):
+		self.message = "TBFW could not load plugin because of unsupported target."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
 
 class InvalidPluginSyntaxError(Exception):
-	raise SyntaxError("TBFW could not load plugin because of invalid syntax.")
+	def __init__(self):
+		self.message = "TBFW could not load plugin because of invalid syntax."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
 
 class InvalidPluginScheduleError(Exception):
-	raise NotImplementedError("TBFW could not load plugin because of unsupported `HOUR` and `MINUTE` and so on.")
+	def __init__(self):
+		self.message = "TBFW could not load plugin because of unsupported `HOUR` and `MINUTE` and so on."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
 
 class TooManyArgmentsForPluginError(Exception):
-	raise NotImplementedError("TBFW could not load plugin because too many argments were required.")
+	def __init__(self):
+		self.message = "TBFW could not load plugin because too many argments were required."
+		Exception.__init__(self, self.message)
+
+	def __str__(self):
+		return self.message
