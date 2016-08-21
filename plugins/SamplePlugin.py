87de3d2ef3c16d171c46700b5ad7de2d6dcf88c9
Nep <spica@starry.blue>
2016-08-21 22:09:59 +0900

    Add: sample plugin

--- plugins/SamplePlugin.py
+++ plugins/SamplePlugin.py
@@ -1,6 +1,21 @@
 # coding=utf-8
-from .. import TBFW
+import time
 
-class Plugin(TBFW.PluginAPI):
-	def OnTweet(self):
-		pass
+from ..TBFW import TwitterAPI
+
+TARGET = "REPLY"
+ACCOUNT = 0
+
+def getPing(tweetid):
+	created = ((tweetid>>22)+1288834974657)/1000.0
+	now = time.time()
+	return float(now - created)
+
+def do(stream):
+	if "ping" in stream["text"]:
+		replyTo = stream["user"]["screen_name"]
+		ping = getPing(stream["id"])
+
+		API = TwitterAPI(accountId=ACCOUNT)
+		API.update_status("@{0} Pong in {1}s.".format(replyTo, ping))
+
