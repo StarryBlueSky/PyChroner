42bdd854e40dfcad9e13b7863f1a6920a675ba20
Nep <spica@starry.blue>
2016-08-21 12:47:01 +0900

    Add: Userstream connection classes and methods

--- TBFW/twitterapi.py
+++ TBFW/twitterapi.py
@@ -1,6 +1,53 @@
 # coding=utf-8
 import tweepy
+import time
+import threading
+import logging
+import urllib.parse
+logger = logging.getLogger(__name__)
 
 class TwitterAPI:
 	def __init__(self):
 		pass
+
+	def startUserStream(self, n):
+		auth = tweepy.OAuthHandler(Set['twitterSecret'][n]['ck'], Set['twitterSecret'][n]['cs'])
+		auth.set_access_token(Set['twitterSecret'][n]['at'], Set['twitterSecret'][n]['ats'])
+		sn = Set['twitterSecret'][n]['screen_name']
+		while True:
+			try:
+				logger.info('@%sのUserStreamに接続しました。' % sn)
+				UserStream(auth, StreamListener(n, sn)).user_stream()
+			except:
+				logger.warning('@%sのUserStreamから切断されました。10秒後に再接続します。エラーログ: \n%s' % (sn, traceback.format_exc()))
+				time.sleep(10)
+
+class StreamListener(tweepy.StreamListener):
+	def __init__(self, n, sn):
+		self.n = n
+		self.sn = sn
+
+	def on_data(self, raw):
+		t = threading.Thread(target=StreamLine, name='StreamLine', args=(raw, self.n, self.sn))
+		t.start()
+
+	def on_error(self, status_code):
+		logger.warning('Twitter APIエラーが発生しました\n詳細: HTTPステータスコード=%s' % status_code)
+
+class UserStream(tweepy.Stream):
+	def user_stream(self):
+		self.parameters = {
+			"delimited": "length",
+			"replies": "all",
+			"filter_level": "none",
+			"include_followings_activity": "True",
+			"stall_warnings": "True",
+			"with": "followings"
+		}
+		self.headers['Content-type'] = "application/x-www-form-urlencoded"
+		self.scheme = "https"
+		self.host = 'userstream.twitter.com'
+		self.url = '/1.1/user.json'
+		self.body = urllib.parse.urlencode(self.parameters)
+		self.timeout = None
+		self._start(False)
