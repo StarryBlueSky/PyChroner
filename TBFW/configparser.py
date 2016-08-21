5b56387f82df62437686549303ab045f04037c3c
Nep <spica@starry.blue>
2016-08-21 15:16:27 +0900

    Add: config parsing

--- TBFW/configparser.py
+++ TBFW/configparser.py
@@ -1,6 +1,24 @@
 # coding=utf-8
 import json
 
+from TBFW.constant import *
+from TBFW.exceptions import *
+
+
 class ConfigParser:
 	def __init__(self):
-		self.config = json.load(open("config.json"))
+		if not os.path.isfile(pathConfig):
+			json.dump({}, open(pathConfig, "w"))
+
+		try:
+			config = json.load(open(pathConfig))
+		except:
+			raise InvalidConfigSyntax
+
+		if not "accounts" in config:
+			raise NoAvailableAccountInConfig
+		self.accounts = config["accounts"]
+
+		self.muteClient = config.get("muteClient", [])
+		self.muteUser = config.get("muteUser", [])
+		self.muteDomain = config.get("muteDomain", [])
