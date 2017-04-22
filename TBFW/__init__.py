6065700f972169538921251d3b362872ee9ad331
Nep <spica@starry.blue>
2017-04-22 15:13:33 +0900

    Optimize: import and __all__

--- TBFW/__init__.py
+++ TBFW/__init__.py
@@ -1,15 +1,15 @@
 # coding=utf-8
-
-import os
-
-from TBFW.enums import *
-from TBFW.constant import *
-from TBFW.exceptions import *
-
 """
 TBFW library
 """
 
-__version__ = "2.0.0"
+from .enums import Path, PluginType, API
+from .plugin import PluginAPI
+from .configparser import Config
+
+config = Config()
+
+__version__ = "3.0.0"
 __author__ = "Nephy Project Team"
 __license__ = "MIT"
+__all__ = ["Path", "PluginType", "API", "PluginAPI", "config"]
