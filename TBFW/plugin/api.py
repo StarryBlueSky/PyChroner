bcc1550865971e1b3ab0f76d37f7cf034a683042
Nep <spica@starry.blue>
2017-04-24 00:26:50 +0900

    Add: PluginAPI Implementation :tada:

--- TBFW/plugin/api.py
+++ TBFW/plugin/api.py
@@ -1,33 +1,64 @@
 # coding=utf-8
-from typing import Callable
+from typing import List, Dict, Callable
+import timeout_decorator
 
-def PluginAPI(pluginType, **kwargs):
-    def decorator(func: Callable[[], None]):
-        def register(*args):
-            # noinspection PyUnresolvedReferences
-            plugin = {
-                "name": func.__name__,
-                "doc": func.__doc__,
-                "variable": {
-                    "count": func.__code__.co_nlocals,
-                    "name": list(func.__code__.co_varnames),
-                    "value": list(args + func.__code__.co_consts[1:])
-                },
-                "timeout": kwargs.get("timeout", None),
-                "priority": kwargs.get("priority", 0),
-                "hours": kwargs.get("hours", []),
-                "minutes": kwargs.get("minutes", []),
-                "multipleHour": kwargs.get("multipleHour", None),
-                "multipleMinute": kwargs.get("multipleMinute", None),
-                "account": kwargs.get("account", None),
-                "ratio": kwargs.get("ratio", 1),
-                "permissions": kwargs.get("permissions", [])
-                # {"plugin": "getPingByStatusID", "action": "deny", "users": ["TwitterJP"], "domain": ["following"]}
-            }
+from .utils import getPluginArgumentCount
+from ..enums import PluginType
+from ..exceptions.plugin import TooManyArgmentsForPluginError, TimeRelatedArgumentsError, TimedOut
 
-            print(pluginType.name)
-            print(plugin)
-            print(kwargs)
-            func(*args)
+
+def PluginAPI(pluginType: PluginType, timeout: int=None, priority: int=None,
+              hours: int=None, minutes: int=None, multipleHour: int=None, multipleMinute: int=None,
+              account: str=None, ratio: int=None, permissions: List[Dict]=None):
+    """
+    decorator implementation of plugin metainfo
+    :param pluginType: (PluginType) target plugin type
+    :param timeout: seconds to stop plugin execution
+    :param priority: priority to execute plugin (min 0)
+    :param hours: 
+    :param minutes: 
+    :param multipleHour: 
+    :param multipleMinute: 
+    :param account: attached account name
+    :param ratio: possibility of 1/n to execute plugin
+    :param permissions: {"action": "deny", "user_id": [10000000], "user_sn": ["TwitterJP"], "domain": ["following"]}
+    :return: 
+    """
+
+    if pluginType is not PluginType.Schedule and any([hours, minutes, multipleHour, multipleMinute]):
+        raise TimeRelatedArgumentsError(
+            "Time related arguments are only able "
+            "to be set when pluginType is not PluginType.Schedule."
+        )
+
+    def decorator(func: Callable):
+        def register(*args) -> Callable:
+            if getPluginArgumentCount(pluginType) != len(args):
+                raise TooManyArgmentsForPluginError(
+                    f"TBFW could not load plugin "
+                    f"because this function takes too many argments."
+                )
+
+        if timeout and pluginType is not PluginType.Thread:
+            register = timeout_decorator.timeout(timeout, timeout_exception=TimedOut)(register)
+
+        setattr(register, "__meta__", {
+            "type": pluginType,
+            "priority": priority or 0,
+            "ratio": ratio or 1,
+            "hours": hours or [],
+            "minutes": minutes or [],
+            "multipleHour": multipleHour,
+            "multipleMinute": multipleMinute,
+            "permissions": permissions or [],
+            "account": account,
+            "timeout": timeout,
+
+            "functionName": func.__name__,
+            "doc": func.__doc__,
+            "variablesCount": func.__code__.co_nlocals,
+            "variablesName": list(func.__code__.co_varnames),
+            "variables": list(func.__code__.co_consts[1:]),
+        })
         return register
     return decorator
