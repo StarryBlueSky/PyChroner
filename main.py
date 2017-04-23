f92647d35c1c733ca9fe3fcc996dba42dbe26916
Nep <spica@starry.blue>
2017-04-23 19:27:28 +0900

    Fix: start script

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # coding=utf-8
-import traceback
-
-from TBFW.core import Core as Bot
+from TBFW import Bot
 
 if __name__ == "__main__":
-	try:
-		Bot.run()
-	except Exception:
-		print(traceback.format_exc())
+    bot = Bot()
+
+    bot.run()
