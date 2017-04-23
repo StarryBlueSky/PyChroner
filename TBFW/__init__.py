7643797e1e3081a081a6c5f62b919d9c3273e475
Nep <spica@starry.blue>
2017-04-23 19:13:46 +0900

    Add: Bot symbol

--- TBFW/__init__.py
+++ TBFW/__init__.py
@@ -3,6 +3,7 @@
 TBFW library
 """
 
+from .core import Core as Bot
 from .enums import PluginType, API
 from .plugin.api import PluginAPI
 from .configparser import Config
@@ -12,4 +13,4 @@ config = Config()
 __version__ = "3.0.0"
 __author__ = "Nephy Project Team"
 __license__ = "MIT"
-__all__ = ["Path", "PluginType", "API", "PluginAPI", "config"]
+__all__ = ["Path", "PluginType", "API", "PluginAPI", "config", "Bot"]
