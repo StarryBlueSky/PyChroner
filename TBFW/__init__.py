8f0e30501a90246ebd8d21ce9868da4718c71c58
Nep <spica@starry.blue>
2017-04-23 21:40:05 +0900

    Optimize: namespace for module

--- TBFW/__init__.py
+++ TBFW/__init__.py
@@ -8,9 +8,7 @@ from .enums import PluginType, API
 from .plugin.api import PluginAPI
 from .configparser import Config
 
-config = Config()
-
 __version__ = "3.0.0"
 __author__ = "Nephy Project Team"
 __license__ = "MIT"
-__all__ = ["Path", "PluginType", "API", "PluginAPI", "config", "Bot"]
+__all__ = ["PluginType", "API", "PluginAPI", "Config", "Bot"]
