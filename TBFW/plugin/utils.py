87f48651c64153081bcdfa90e4ca2bb08ecc2b44
Nep <spica@starry.blue>
2017-04-24 00:28:27 +0900

    Add: getPluginArgumentCount function

--- TBFW/plugin/utils.py
+++ TBFW/plugin/utils.py
@@ -3,8 +3,12 @@ import re
 from hashlib import sha1
 
 from typing.re import Pattern
+from ..enums import PluginType
 
 pluginFilePattern: Pattern = re.compile("^.+[.]py$")
 
 def getPluginId(path: str):
     return sha1(path.encode()).hexdigest()
+
+def getPluginArgumentCount(pluginType: PluginType) -> int:
+    return 1 if pluginType in [PluginType.Reply, PluginType.Timeline, PluginType.Event, PluginType.DM] else 0
