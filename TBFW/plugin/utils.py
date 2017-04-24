a02ee42897fa8f1c18a9354e9a190c860926f020
Nep <spica@starry.blue>
2017-04-24 19:01:51 +0900

    Move: willExecute

--- TBFW/plugin/utils.py
+++ TBFW/plugin/utils.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import random
 import re
 from hashlib import sha1
 
@@ -12,3 +13,6 @@ def getPluginId(path: str):
 
 def getPluginArgumentCount(pluginType: PluginType) -> int:
     return 1 if pluginType in [PluginType.Reply, PluginType.Timeline, PluginType.Event, PluginType.DM] else 0
+
+def willExecute(ratio: int) -> bool:
+    return random.randint(1, ratio) == 1
