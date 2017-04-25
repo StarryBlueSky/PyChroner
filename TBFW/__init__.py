7512c677a465d8503c685eef22ea27786a03e877
Nep <spica@starry.blue>
2017-04-25 09:54:19 +0900

    Add: importModule function to import submodules

--- TBFW/__init__.py
+++ TBFW/__init__.py
@@ -7,8 +7,9 @@ from .core import Core as Bot
 from .enums import PluginType, API
 from .plugin.api import PluginAPI
 from .configparser import Config
+from .submodules import importModule
 
 __version__ = "3.0.0"
 __author__ = "Nephy Project Team"
 __license__ = "MIT"
-__all__ = ["PluginType", "API", "PluginAPI", "Config", "Bot"]
+__all__ = ["PluginType", "API", "PluginAPI", "Config", "Bot", "importModule"]
