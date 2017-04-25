749d5d58161decda5e69c572799cd2fb6644bca5
Nep <spica@starry.blue>
2017-04-25 11:42:45 +0900

    Delete: ConvertedObject class

--- TBFW/utils.py
+++ TBFW/utils.py
@@ -10,22 +10,6 @@ from typing import Dict, Match, List
 from .exceptions.config import InvalidLiteralError
 
 
-class ConvertedObject:
-    pass
-
-def convertDictToObject(x: Dict) -> object:
-    y: ConvertedObject = ConvertedObject()
-
-    for k, v in x.items():
-        if isinstance(v, dict):
-            v: Dict = convertDictToObject(v)
-
-        if not isSafeLiteral(k):
-            raise InvalidLiteralError(f"`{k}` is invalid literal.")
-        setattr(y, k, v)
-
-    return y
-
 def listAttr(x: object) -> list:
     return [y for y in dir(x) if not y.startswith("__") and not y.endswith("__")]
 
