62193db8805325776795cdaf644767a2b8c0955b
Nep <spica@starry.blue>
2017-07-18 15:45:33 +0900

    Fix: Opusがない場合 ロードを中止

--- pychroner/discord/__init__.py
+++ pychroner/discord/__init__.py
@@ -1,5 +1,12 @@
 # coding=utf-8
+from logging import getLogger
+
 import discord
 
+logger = getLogger(__name__)
+
 if not discord.opus.is_loaded():
-    discord.opus.load_opus("opus")
+    try:
+        discord.opus.load_opus("opus")
+    except:
+        logger.warning("Discord.py could not load Opus library. So PyChroner may not be connecting Discord VC like MusicBot.")
