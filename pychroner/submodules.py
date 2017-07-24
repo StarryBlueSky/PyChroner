7caf9cf8ba1610b0bdbc173b6ab9eb24f21a9fb8
Nep <spica@starry.blue>
2017-07-25 00:22:14 +0900

    Fix: importModuleの返り値の型を適切なものに変更

--- pychroner/submodules.py
+++ pychroner/submodules.py
@@ -2,15 +2,16 @@
 import importlib
 import os
 import sys
+from types import ModuleType
 
 submodulesDir: str = "submodules"
 
-def importModule(name: str) -> object:
+def importModule(name: str) -> ModuleType:
     for directory in os.listdir(submodulesDir):
         sys.path.append(f"{submodulesDir}/{directory}")
 
     try:
-        submodule: object = importlib.import_module(name)
+        submodule: ModuleType = importlib.import_module(name)
         return submodule
     except:
         raise ImportError(f"No named submodule {name}")
