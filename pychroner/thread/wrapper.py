5f8974ee7406dc26a0e8303f37fa448867feedf1
Nep <spica@starry.blue>
2017-07-26 18:46:37 +0900

    Fix: pluginApiにプラグイン情報が載らない問題を修正

--- pychroner/thread/wrapper.py
+++ pychroner/thread/wrapper.py
@@ -18,6 +18,7 @@ class ThreadWrapper:
     def wrap(self, plugin: Plugin, *args) -> None:
         try:
             api = PluginAPI(self.core)
+            api.plugin = plugin
             args = [api] + list(args)
             getattr(plugin.module, plugin.meta.functionName)(*args)
             if plugin.meta.type is PluginType.Schedule:
