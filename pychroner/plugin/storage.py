6f983cfbc4eca402741b0e6fb7260d7e61016636
Nep <spica@starry.blue>
2017-07-26 18:56:18 +0900

    Update: LocalStorageの型を明示化

--- pychroner/plugin/storage.py
+++ pychroner/plugin/storage.py
@@ -1,15 +1,16 @@
 # coding=utf-8
+from typing import Dict
 
 class LocalStorage:
-    def __init__(self):
-        self.__storage = {}
+    def __init__(self) -> None:
+        self.__storage: Dict[str] = {}
 
-    def __get(self, key: str) -> object:
+    def __get(self, key: str) -> Dict[str]:
         if key not in self.__storage:
             self.__storage[key] = {}
         return self.__storage[key]
 
-    def get(self, plugin) -> object:
+    def get(self, plugin) -> Dict[str]:
         return self.__get(plugin.meta.id)
 
     def __clear(self, key: str) -> bool:
